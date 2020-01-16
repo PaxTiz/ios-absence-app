45dcaad0a09113db2ae23033021e56a5e63620a6
Valentin Cernuta <valentin_cernuta@ens.univ-artois.fr>
2020-01-16 17:23:16 +0100

    fix: scroll view for every semesters fix: unstage .idea folder

--- First iOS App/ContentView.swift
+++ First iOS App/ContentView.swift
@@ -11,57 +11,49 @@ import SwiftUI
 struct ContentView: View {
 	
 	@State private var selection = 0
+	@State private var materialAction: Bool = false
+	
 	private let semesters: [Semester] = [
 		Semester(id: 1, materials: [
 			Material(name: "SE", totalHours: 45),
 			Material(name: "PWEB", totalHours: 60),
 			Material(name: "Anglais", totalHours: 45),
+			Material(name: "SE2", totalHours: 45),
+			Material(name: "SE2", totalHours: 45),
+			Material(name: "Orga", totalHours: 60),
+			Material(name: "Anglais", totalHours: 45),
+			Material(name: "PWEB", totalHours: 45),
 		]),
 		Semester(id: 2, materials: [
 			Material(name: "SE2", totalHours: 45),
 			Material(name: "Orga", totalHours: 60),
 			Material(name: "Anglais", totalHours: 45),
+			Material(name: "PWEB", totalHours: 45),
 		]),
 		Semester(id: 3, materials: [
 			Material(name: "SE3", totalHours: 45),
 			Material(name: "PWEB", totalHours: 60),
 			Material(name: "GSI", totalHours: 45),
+			Material(name: "Droit", totalHours: 45)
 		]),
 		Semester(id: 4, materials: [
 			Material(name: "RX3", totalHours: 45),
 			Material(name: "PWEB2", totalHours: 60),
 			Material(name: "ProgMobile", totalHours: 45),
+			Material(name: "Anglais", totalHours: 45)
 		])
 	]
-
-	@State private var materialAction: Bool = false
 	
 	var body: some View {
-		
-		NavigationView {
-			TabView(selection: $selection) {
-				
-				ForEach(semesters, id: \.self) { sm in
-					Home(semester: sm).tabItem {
-						Image(systemName: "\(sm.identifier).circle")
-						Text("Semestre " + String(sm.identifier))
-					}.tag(sm.identifier - 1)
-				}.padding(15)
-				
-			}.navigationBarTitle("Absences", displayMode: .inline)
-				.navigationBarItems(
-					// Bouton de droite
-					trailing: Button(action: {
-						self.materialAction = true
-					}){
-						Image(systemName: "plus").foregroundColor(Color.blue)
-						Text("Matière")
-					}.sheet(isPresented: $materialAction) {
-						MaterialModal(semester: self.selection)
-					}
-			)
+		TabView(selection: $selection) {
+			ForEach(semesters, id: \.self) { sm in
+				Home(semester: sm).tabItem {
+					Image(systemName: "\(sm.identifier).circle")
+					Text("Semestre \(sm.identifier)")
+				}.tag(sm.identifier - 1)
+			}
+			
 		}
-		
 	}
 	
 }
--- First iOS App/Views/Home.swift
+++ First iOS App/Views/Home.swift
@@ -9,8 +9,81 @@
 import SwiftUI
 
 struct Home: View {
-	
+
 	private var current: Semester
+	@State private var materialAction: Bool = false
+
+
+	init(semester: Semester) {
+		current = semester
+	}
+
+    var body: some View {
+		NavigationView {
+			ScrollView {
+				VStack {
+					// Bouton pour mettre le semestre courant par défaut quand on lance l'app
+					Button(action: {}) {
+						Text("Semestre par défaut")
+							.fontWeight(.medium)
+							.padding(10)
+							.frame(maxWidth: .infinity)
+							.foregroundColor(Color.white)
+							.background(Color.blue)
+							.cornerRadius(10, antialiased: true)
+							.font(.custom("Avenir Next Condensed", size: 18))
+					}
+					
+					VStack(spacing: 10) {
+						Text("Matières informatiques")
+							.font(.title)
+							.fontWeight(.bold)
+							.frame(maxWidth: .infinity, alignment: .leading)
+							.padding(.top, 10)
+						
+						// Grille avec toutes les matières
+						ForEach(current.materials, id: \.self) { ma in
+							HStack(spacing: 10) {
+								Card(material: ma)
+							}
+						}
+						
+						Text("Matières générales")
+							.font(.title)
+							.fontWeight(.bold)
+							.frame(maxWidth: .infinity, alignment: .leading)
+							.padding(.top, 10)
+						
+						// Grille avec toutes les matières
+						ForEach(current.materials, id: \.self) { ma in
+							HStack(spacing: 10) {
+								Card(material: ma)
+							}
+						}
+					}
+				}
+			}.padding(EdgeInsets.init(top: 0, leading: 10, bottom: 10, trailing: 10))
+				.navigationBarTitle("Semestre \(current.identifier)")
+					.navigationBarItems(
+						// Bouton de droite
+						trailing: Button(action: {
+							self.materialAction = true
+						}){
+							Image(systemName: "plus").foregroundColor(Color.blue)
+							Text("Matière")
+						}.sheet(isPresented: $materialAction) {
+							MaterialModal(semester: self.current.identifier)
+						}
+				)
+			}
+		}
+}
+
+/// View for a material card
+/// - Parameter material: the material to show
+struct Card: View {
+	
+	private let material: Material
 	private var colors: [[Color]] = [
 		// Vert
 		[Color.init(red: 0.46, green: 0.72, blue: 0.32), Color.init(red: 0.55, green: 0.76, blue: 0.44)],
@@ -20,54 +93,48 @@ struct Home: View {
 		[Color.init(red: 1, green: 0.72, blue: 0.37), Color.init(red: 0.93, green: 0.56, blue: 0.01)],
 	]
 	
-	init(semester: Semester) {
-		current = semester
+	init(material: Material) {
+		self.material = material
 	}
- 
-    var body: some View {
-		VStack(spacing: 10) {
-			ForEach(current.materials, id: \.self) { ma in
-				HStack(spacing: 10) {
-					ForEach(0..<2) { x in
-						VStack(alignment: .leading, spacing: 5) {
-							HStack {
-								Text(ma.name)
-									.font(.custom("Avenir Next Condensed", size: 22))
-									.foregroundColor(Color.white)
-									
-								Spacer()
-								
-								Text(String(ma.absences))
-									.foregroundColor(Color.white)
-									.font(.custom("Avenir Next Condensed", size: 22))
-							}
-							
-							HStack {
-								Image(systemName: "timer").foregroundColor(Color.white)
-								Text("Restantes")
-									.foregroundColor(Color.white)
-									.font(.custom("Avenir Next Condensed", size: 20))
-									.fontWeight(.medium)
-								
-								Spacer()
-								
-								Text(String(ma.absences))
-									.foregroundColor(Color.white)
-									.font(.custom("Avenir Next Condensed", size: 22))
-									.fontWeight(.medium)
-							}
-						}.padding().background(
-								RadialGradient(gradient:
-									Gradient(
-										colors: self.chooseColor(nb: ma.absences)),
-											   center: .center, startRadius: 0, endRadius: 180
-							)).cornerRadius(10)
-					}
-				}
+	
+	var body: some View {
+		VStack(spacing: 5) {
+			HStack {
+				Text(material.name)
+					.font(.custom("Avenir Next Condensed", size: 22))
+					.foregroundColor(Color.white)
+				
+				Spacer()
+				
+				Text(String(material.absences))
+					.foregroundColor(Color.white)
+					.font(.custom("Avenir Next Condensed", size: 22))
 			}
-			Spacer()
-		}
-    }
+			
+			HStack {
+				Image(systemName: "timer").foregroundColor(Color.white)
+				Text("Restantes")
+					.foregroundColor(Color.white)
+					.font(.custom("Avenir Next Condensed", size: 20))
+					.fontWeight(.medium)
+				
+				Spacer()
+				
+				Text(String(material.absences))
+					.foregroundColor(Color.white)
+					.font(.custom("Avenir Next Condensed", size: 22))
+					.fontWeight(.medium)
+			}
+		}.padding()
+		.background(
+			RadialGradient(gradient:
+				Gradient(
+					colors: self.chooseColor(nb: self.material.absences)),
+				   center: .center,
+				   startRadius: 0,
+				   endRadius: 180
+		)).cornerRadius(10)
+	}
 	
 	func chooseColor(nb: Int) -> [Color] {
 		if nb == 0 {
@@ -78,7 +145,6 @@ struct Home: View {
 		
 		return self.colors[1]
 	}
-	
 }
 
 struct Home_Previews: PreviewProvider {
@@ -87,6 +153,7 @@ struct Home_Previews: PreviewProvider {
 			Material(name: "SE", totalHours: 45),
 			Material(name: "PWEB", totalHours: 60),
 			Material(name: "Anglais", totalHours: 45),
+			Material(name: "SE2", totalHours: 45)
 		]))
     }
 }
